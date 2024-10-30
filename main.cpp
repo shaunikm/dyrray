a96968f010118725e68e9df26b161a22bbe71091
Shaunik Musukula <68877932+shaunikm@users.noreply.github.com>
2024-10-30 10:58:51 -0700

    edit commnts

--- main.cpp
+++ main.cpp
@@ -5,55 +5,54 @@
 #include "dyrray.hpp"
 
 int main() {
-    // 1. test for default construction
+    // 1. default construction
     Dyrray<int> array;
     assert(array.get_size() == 0);
     assert(array.get_capacity() == 0);
     std::cout << "Default constructor test passed.\n";
 
-    // 2. test push_back and dynamic resizing
+    // 2. push_back and dynamic resizing
     array.push_back(10);
     assert(array.get_size() == 1);
     assert(array[0] == 10);
     std::cout << "First push_back test passed.\n";
     std::cout << "Current capacity: " << array.get_capacity() << std::endl;
 
-    // Add more elements to test capacity growth
     for (int i = 0; i < 100; ++i) {
         array.push_back(i);
     }
-    assert(array.get_size() == 101); // Including the initial push_back
+    assert(array.get_size() == 101);
     assert(array[100] == 99);
     std::cout << "Push_back and dynamic resizing test passed.\n";
     std::cout << "Current capacity: " << array.get_capacity() << std::endl;
 
-    // 3. Test access with at()
+    // 3. access with at()
     try {
-        int val = array.at(100); // Should work
+        int val = array.at(100);
         assert(val == 99);
-        array.at(101); // Should throw an exception
+        array.at(101);
     } catch (const std::out_of_range& e) {
         std::cout << "Access test with at() passed: " << e.what() << '\n';
     }
 
-    // 4. Test pop_back and shrink-to-fit behavior
+    // 4. pop_back and shrink-to-fit behavior
     array.pop_back();
     assert(array.get_size() == 100);
     std::cout << "Pop_back test passed.\n";
 
-    // 5. Test clear function
+    // 5. clear
     array.clear();
     assert(array.get_size() == 0);
     std::cout << "Clear function test passed.\n";
     std::cout << "Current capacity: " << array.get_capacity() << std::endl;
 
-    // 6. Test move constructor
+    // 6. move constructor
     Dyrray<int> array2(std::move(array));
-    assert(array.get_size() == 0);   // array should be empty after move
-    assert(array2.get_size() == 0);  // array2 should also be empty
+    assert(array.get_size() == 0);
+    assert(array2.get_size() == 0);
     std::cout << "Move constructor test passed.\n";
 
-    // 7. Test copy assignment
+    // 7. copy assignment
     for (int i = 0; i < 10; ++i) {
         array2.push_back(i);
     }
